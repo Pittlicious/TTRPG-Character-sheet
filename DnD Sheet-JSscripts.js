fa38ec24143e1933b6ea984e887241fe59b4e399
Pittlicious <thelen.kayser@gmail.com>
2024-09-23 17:37:50 +0200

    user input ability

--- DnD Sheet-JSscripts.js
+++ DnD Sheet-JSscripts.js
@@ -1,10 +1,9 @@
 // Select all elements with the class 'circle'
 const circles = document.querySelectorAll('.circle');
 
-// Add a click event listener to each circle
+// toggle stress circle
 circles.forEach(circle => {
     circle.addEventListener('click', () => {
-        // Toggle the 'red' class on click
         circle.classList.toggle('red');
     });
 });
