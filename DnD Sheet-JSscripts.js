4c01c015de1e3ae7ca09d8408899d91c0aafb70a
Pittlicious <thelen.kayser@gmail.com>
2024-10-18 13:45:23 +0200

    added extra die

--- DnD Sheet-JSscripts.js
+++ DnD Sheet-JSscripts.js
@@ -16,18 +16,30 @@ document.getElementById("d20").addEventListener("click", function()
     document.getElementById("result").textContent = roll;
 })
 
+// d12
+document.getElementById("d12")addEventListener("click", function()
+{
+    let roll =  Math.floor(Math.random() * 12) +1;
+    this.document.getElementById("result").textContent = roll;
+})
+
+// d8
+document.getElementById("d8")addEventListener("click", function()
+{
+    let roll =  Math.floor(Math.random() * 8) +1;
+    this.document.getElementById("result").textContent = roll;
+})
+
 // d6
 document.getElementById("d6").addEventListener("click", function()
 {
     let roll = Math.floor(Math.random() * 6) +1;
     document.getElementById("result").textContent = roll;
-}
-)
+})
 
 // d4
 document.getElementById("d4").addEventListener("click", function()
 {
     let roll = Math.floor(Math.random() * 4) +1;
     document.getElementById("result").textContent = roll;
-}
-)
+})
